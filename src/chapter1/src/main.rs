95c1a21fcfd41910e5d64c6575678aae0968fd14
Mark Shevchenko <mark-progmsk@yandex-team.ru>
2022-09-06 14:25:41 +0300

    Implement linear search for array

---
+++ src/chapter1/src/main.rs
@@ -0,0 +1,18 @@
+fn search(items: &[i32], value: i32) -> Option<usize> {
+    for i in 0..items.len() {
+        if items[i] == value {
+            return Some(i);
+        }
+    }
+
+    return None;
+}
+
+fn main() {
+    let items = [0, 12, 6, 18, 3, 4];
+    let index1 = find(&items, 6);
+    println!("find({:?}, {}) = {:?}", items, 6, index1);
+
+    let index2 = find(&items, 5);
+    println!("find({:?}, {}) = {:?}", items, 5, index2);
+}
