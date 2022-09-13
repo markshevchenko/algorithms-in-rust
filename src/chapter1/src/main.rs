872fd575fd70b7d0b8df83898a526fc174590843
Mark Shevchenko <markshevchenko@gmail.com>
2022-09-13 15:59:54 +0300

    Implement binary search with tests

--- src/chapter1/src/main.rs
+++ src/chapter1/src/main.rs
@@ -14,17 +14,17 @@ fn search(items: &[i32], value: i32) -> Option<usize> {
 mod search_should {
     #[test]
     fn return_none_when_empty_items() {
-        assert_eq!(None, super::search(&vec![], 1));
+        assert_eq!(None, super::search(&[], 1));
     }
 
     #[test]
     fn return_none_when_items_doesnt_contain_value() {
-        assert_eq!(None, super::search(&vec![2, 3, 4], 1));
+        assert_eq!(None, super::search(&[2, 3, 4], 1));
     }
 
     #[test]
     fn return_some_index_when_items_contains_value() {
-        assert_eq!(Some(2), super::search(&vec![3, 2, 1, 0], 1));
+        assert_eq!(Some(2), super::search(&[3, 2, 1, 0], 1));
     }
 }
 
@@ -42,17 +42,17 @@ fn contains(items: &[i32], value: i32) -> bool {
 mod contains_should {
     #[test]
     fn return_false_when_empty_items() {
-        assert_eq!(false, super::contains(&vec![], 1));
+        assert_eq!(false, super::contains(&[], 1));
     }
 
     #[test]
     fn return_false_when_items_234_dont_contain_1() {
-        assert_eq!(false, super::contains(&vec![2, 3, 4], 1));
+        assert_eq!(false, super::contains(&[2, 3, 4], 1));
     }
 
     #[test]
     fn return_true_when_items_3210_contain_1() {
-        assert!(super::contains(&vec![3, 2, 1, 0], 1));
+        assert!(super::contains(&[3, 2, 1, 0], 1));
     }
 }
 
@@ -76,17 +76,17 @@ fn min(items: &[i32]) -> Option<i32> {
 mod min_should {
     #[test]
     fn return_none_when_empty_items() {
-        assert_eq!(None, super::min(&vec![]));
+        assert_eq!(None, super::min(&[]));
     }
 
     #[test]
     fn return_some_1_when_items_are_1() {
-        assert_eq!(Some(1), super::min(&vec![1]));
+        assert_eq!(Some(1), super::min(&[1]));
     }
 
     #[test]
     fn return_some_1_when_items_are_23145() {
-        assert_eq!(Some(1), super::min(&vec![2, 3, 1, 4, 5]));
+        assert_eq!(Some(1), super::min(&[2, 3, 1, 4, 5]));
     }
 }
 
@@ -108,12 +108,12 @@ where T: Copy + AddAssign + Zero {
 mod sum_should {
     #[test]
     fn return_0_when_empty_items() {
-        assert_eq!(0, super::sum(&vec![]));
+        assert_eq!(0, super::sum(&[]));
     }
 
     #[test]
     fn return_10_when_items_are_1234() {
-        assert_eq!(10, super::sum(&vec![1, 2, 3, 4]));
+        assert_eq!(10, super::sum(&[1, 2, 3, 4]));
     }
 }
 
@@ -131,12 +131,12 @@ fn prod(items: &[i32]) -> i32 {
 mod prod_should {
     #[test]
     fn return_1_when_empty_items() {
-        assert_eq!(1, super::prod(&vec![]));
+        assert_eq!(1, super::prod(&[]));
     }
 
     #[test]
     fn return_24_when_items_are_1234() {
-        assert_eq!(24, super::prod(&vec![1, 2, 3, 4]));
+        assert_eq!(24, super::prod(&[1, 2, 3, 4]));
     }
 }
 
@@ -152,22 +152,84 @@ fn average(items: &[f64]) -> Option<f64> {
 mod average_should {
     #[test]
     fn return_none_when_empty_items() {
-        assert_eq!(None, super::average(&vec![]));
+        assert_eq!(None, super::average(&[]));
     }
 
     #[test]
     fn return_some_3_when_items_are_12345() {
-        assert_eq!(Some(3.0), super::average(&vec![1.0, 2.0, 3.0, 4.0, 5.0]));
+        assert_eq!(Some(3.0), super::average(&[1.0, 2.0, 3.0, 4.0, 5.0]));
     }
 }
 
 mod md5;
 
+fn binary_search(items: &[i32], value: i32) -> Option<usize> {
+    let mut left = 0usize;
+    let mut right = items.len();
+
+    loop {
+        if left == right {
+            return None
+        }
+
+        let middle = left + (right - left)/2;
+        if value == items[middle] {
+            return Some(middle)
+        }
+
+        if value < items[middle] {
+            right = middle;
+        } else {
+            left = middle + 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod binary_search_should {
+    use crate::binary_search;
+
+    #[test]
+    fn return_none_when_empty_items() {
+        let actual = binary_search(&[], 7);
+
+        assert_eq!(None, actual);
+    }
+
+    #[test]
+    fn return_some_0_when_items_are_7_and_value_is_7() {
+        let actual = binary_search(&[7], 7);
+
+        assert_eq!(Some(0), actual);
+    }
+
+    #[test]
+    fn return_none_when_items_are_7_and_value_is_4() {
+        let actual = binary_search(&[7], 4);
+
+        assert_eq!(None, actual);
+    }
+
+    #[test]
+    fn return_some_3_when_items_are_13579_and_value_is_7() {
+        let actual = binary_search(&[1, 3, 5, 7, 9], 7);
+
+        assert_eq!(Some(3), actual);
+    }
+
+    #[test]
+    fn return_none_when_items_are_13579_and_value_is_4() {
+        let actual = binary_search(&[1, 3, 5, 7, 9], 4);
+
+        assert_eq!(None, actual);
+    }
+}
+
 fn main() {
-    let empty = Vec::<i32>::new();
+    let empty = [0; 0];
     let items = [0, 12, 6, 18, 3, 4];
 
-    let empty_float = Vec::<f64>::new();
+    let empty_float = [0.0; 0];
     let float_items = [0.0, 12.0, 6.0, 18.0, 3.0, 4.0];
 
     println!("search({:?}, {}) = {:?}", items, 6, search(&items, 6));
