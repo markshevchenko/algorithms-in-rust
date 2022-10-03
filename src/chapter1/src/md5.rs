cfda7adf660fead2162c54c8e1562938a2162994
Mark Shevchenko <markshevchenko@gmail.com>
2022-10-03 14:32:05 +0300

    Refactor

--- src/chapter1/src/md5.rs
+++ src/chapter1/src/md5.rs
@@ -1,120 +1,7 @@
 use std::mem::{transmute};
 use std::ops::AddAssign;
 
-// MD5 algorithm has four auxiliary functions — f, g, h, and i, that mix three u32 words.
-fn f(x: u32, y: u32, z: u32) -> u32 {
-    x & y | !x & z
-}
-
-#[cfg(test)]
-mod f_should {
-    // x y z  x & y | !x & z
-    // 0 0 0        0
-    // 0 0 1        1
-    // 0 1 0        0
-    // 0 1 1        1
-    // 1 0 0        0
-    // 1 0 1        0
-    // 1 1 0        1
-    // 1 1 1        1
-    #[test]
-    fn match_truth_table() {
-        assert_eq!(0x00000000, super::f(0x00000000, 0x00000000, 0x00000000));
-        assert_eq!(0xffffffff, super::f(0x00000000, 0x00000000, 0xffffffff));
-        assert_eq!(0x00000000, super::f(0x00000000, 0xffffffff, 0x00000000));
-        assert_eq!(0xffffffff, super::f(0x00000000, 0xffffffff, 0xffffffff));
-        assert_eq!(0x00000000, super::f(0xffffffff, 0x00000000, 0x00000000));
-        assert_eq!(0x00000000, super::f(0xffffffff, 0x00000000, 0xffffffff));
-        assert_eq!(0xffffffff, super::f(0xffffffff, 0xffffffff, 0x00000000));
-        assert_eq!(0xffffffff, super::f(0xffffffff, 0xffffffff, 0xffffffff));
-    }
-}
-
-fn g(x: u32, y: u32, z: u32) -> u32 {
-    x & z | y & !z
-}
-
-#[cfg(test)]
-mod g_should {
-    // x y z  x & z | y & !z
-    // 0 0 0        0
-    // 0 0 1        0
-    // 0 1 0        1
-    // 0 1 1        0
-    // 1 0 0        0
-    // 1 0 1        1
-    // 1 1 0        1
-    // 1 1 1        1
-    #[test]
-    fn match_truth_table() {
-        assert_eq!(0x00000000, super::g(0x00000000, 0x00000000, 0x00000000));
-        assert_eq!(0x00000000, super::g(0x00000000, 0x00000000, 0xffffffff));
-        assert_eq!(0xffffffff, super::g(0x00000000, 0xffffffff, 0x00000000));
-        assert_eq!(0x00000000, super::g(0x00000000, 0xffffffff, 0xffffffff));
-        assert_eq!(0x00000000, super::g(0xffffffff, 0x00000000, 0x00000000));
-        assert_eq!(0xffffffff, super::g(0xffffffff, 0x00000000, 0xffffffff));
-        assert_eq!(0xffffffff, super::g(0xffffffff, 0xffffffff, 0x00000000));
-        assert_eq!(0xffffffff, super::g(0xffffffff, 0xffffffff, 0xffffffff));
-    }
-}
-
-fn h(x: u32, y: u32, z: u32) -> u32 {
-    x ^ y ^ z
-}
-
-#[cfg(test)]
-mod h_should {
-    // x y z  x ^ y ^ z
-    // 0 0 0      0
-    // 0 0 1      1
-    // 0 1 0      1
-    // 0 1 1      0
-    // 1 0 0      1
-    // 1 0 1      0
-    // 1 1 0      0
-    // 1 1 1      1
-    #[test]
-    fn match_truth_table() {
-        assert_eq!(0x00000000, super::h(0x00000000, 0x00000000, 0x00000000));
-        assert_eq!(0xffffffff, super::h(0x00000000, 0x00000000, 0xffffffff));
-        assert_eq!(0xffffffff, super::h(0x00000000, 0xffffffff, 0x00000000));
-        assert_eq!(0x00000000, super::h(0x00000000, 0xffffffff, 0xffffffff));
-        assert_eq!(0xffffffff, super::h(0xffffffff, 0x00000000, 0x00000000));
-        assert_eq!(0x00000000, super::h(0xffffffff, 0x00000000, 0xffffffff));
-        assert_eq!(0x00000000, super::h(0xffffffff, 0xffffffff, 0x00000000));
-        assert_eq!(0xffffffff, super::h(0xffffffff, 0xffffffff, 0xffffffff));
-    }
-}
-
-fn i(x: u32, y: u32, z: u32) -> u32 {
-    y ^ (x | !z)
-}
-
-#[cfg(test)]
-mod i_should {
-    // x y z  y ^ (x | !z)
-    // 0 0 0    1
-    // 0 0 1    0
-    // 0 1 0    0
-    // 0 1 1    1
-    // 1 0 0    1
-    // 1 0 1    1
-    // 1 1 0    0
-    // 1 1 1    0
-    #[test]
-    fn match_truth_table() {
-        assert_eq!(0xffffffff, super::i(0x00000000, 0x00000000, 0x00000000));
-        assert_eq!(0x00000000, super::i(0x00000000, 0x00000000, 0xffffffff));
-        assert_eq!(0x00000000, super::i(0x00000000, 0xffffffff, 0x00000000));
-        assert_eq!(0xffffffff, super::i(0x00000000, 0xffffffff, 0xffffffff));
-        assert_eq!(0xffffffff, super::i(0xffffffff, 0x00000000, 0x00000000));
-        assert_eq!(0xffffffff, super::i(0xffffffff, 0x00000000, 0xffffffff));
-        assert_eq!(0x00000000, super::i(0xffffffff, 0xffffffff, 0x00000000));
-        assert_eq!(0x00000000, super::i(0xffffffff, 0xffffffff, 0xffffffff));
-    }
-}
-
-/// Contains A, B, C, and D 32-bit words used to compute message digest.
+/// Contains A, B, C, and D parts of message digest.
 #[derive(Clone, PartialEq, Debug)]
 struct Digest {
     pub a: u32,
@@ -161,6 +48,123 @@ mod from_should
     }
 }
 
+// MD5 algorithm has four auxiliary functions — f, g, h, and i, that mix three u32 words.
+/// Implements mixing formula for first round (function `f`)
+fn f(x: u32, y: u32, z: u32) -> u32 {
+    x & y | !x & z
+}
+
+#[cfg(test)]
+mod f_should {
+    // x y z  x & y | !x & z
+    // 0 0 0        0
+    // 0 0 1        1
+    // 0 1 0        0
+    // 0 1 1        1
+    // 1 0 0        0
+    // 1 0 1        0
+    // 1 1 0        1
+    // 1 1 1        1
+    #[test]
+    fn match_truth_table() {
+        assert_eq!(0x00000000, super::f(0x00000000, 0x00000000, 0x00000000));
+        assert_eq!(0xffffffff, super::f(0x00000000, 0x00000000, 0xffffffff));
+        assert_eq!(0x00000000, super::f(0x00000000, 0xffffffff, 0x00000000));
+        assert_eq!(0xffffffff, super::f(0x00000000, 0xffffffff, 0xffffffff));
+        assert_eq!(0x00000000, super::f(0xffffffff, 0x00000000, 0x00000000));
+        assert_eq!(0x00000000, super::f(0xffffffff, 0x00000000, 0xffffffff));
+        assert_eq!(0xffffffff, super::f(0xffffffff, 0xffffffff, 0x00000000));
+        assert_eq!(0xffffffff, super::f(0xffffffff, 0xffffffff, 0xffffffff));
+    }
+}
+
+/// Implements mixing formula for second round (function `g`)
+fn g(x: u32, y: u32, z: u32) -> u32 {
+    x & z | y & !z
+}
+
+#[cfg(test)]
+mod g_should {
+    // x y z  x & z | y & !z
+    // 0 0 0        0
+    // 0 0 1        0
+    // 0 1 0        1
+    // 0 1 1        0
+    // 1 0 0        0
+    // 1 0 1        1
+    // 1 1 0        1
+    // 1 1 1        1
+    #[test]
+    fn match_truth_table() {
+        assert_eq!(0x00000000, super::g(0x00000000, 0x00000000, 0x00000000));
+        assert_eq!(0x00000000, super::g(0x00000000, 0x00000000, 0xffffffff));
+        assert_eq!(0xffffffff, super::g(0x00000000, 0xffffffff, 0x00000000));
+        assert_eq!(0x00000000, super::g(0x00000000, 0xffffffff, 0xffffffff));
+        assert_eq!(0x00000000, super::g(0xffffffff, 0x00000000, 0x00000000));
+        assert_eq!(0xffffffff, super::g(0xffffffff, 0x00000000, 0xffffffff));
+        assert_eq!(0xffffffff, super::g(0xffffffff, 0xffffffff, 0x00000000));
+        assert_eq!(0xffffffff, super::g(0xffffffff, 0xffffffff, 0xffffffff));
+    }
+}
+
+/// Implements mixing formula for third round (function `h`)
+fn h(x: u32, y: u32, z: u32) -> u32 {
+    x ^ y ^ z
+}
+
+#[cfg(test)]
+mod h_should {
+    // x y z  x ^ y ^ z
+    // 0 0 0      0
+    // 0 0 1      1
+    // 0 1 0      1
+    // 0 1 1      0
+    // 1 0 0      1
+    // 1 0 1      0
+    // 1 1 0      0
+    // 1 1 1      1
+    #[test]
+    fn match_truth_table() {
+        assert_eq!(0x00000000, super::h(0x00000000, 0x00000000, 0x00000000));
+        assert_eq!(0xffffffff, super::h(0x00000000, 0x00000000, 0xffffffff));
+        assert_eq!(0xffffffff, super::h(0x00000000, 0xffffffff, 0x00000000));
+        assert_eq!(0x00000000, super::h(0x00000000, 0xffffffff, 0xffffffff));
+        assert_eq!(0xffffffff, super::h(0xffffffff, 0x00000000, 0x00000000));
+        assert_eq!(0x00000000, super::h(0xffffffff, 0x00000000, 0xffffffff));
+        assert_eq!(0x00000000, super::h(0xffffffff, 0xffffffff, 0x00000000));
+        assert_eq!(0xffffffff, super::h(0xffffffff, 0xffffffff, 0xffffffff));
+    }
+}
+
+/// Implements mixing formula for fourth round (function `i`)
+fn i(x: u32, y: u32, z: u32) -> u32 {
+    y ^ (x | !z)
+}
+
+#[cfg(test)]
+mod i_should {
+    // x y z  y ^ (x | !z)
+    // 0 0 0    1
+    // 0 0 1    0
+    // 0 1 0    0
+    // 0 1 1    1
+    // 1 0 0    1
+    // 1 0 1    1
+    // 1 1 0    0
+    // 1 1 1    0
+    #[test]
+    fn match_truth_table() {
+        assert_eq!(0xffffffff, super::i(0x00000000, 0x00000000, 0x00000000));
+        assert_eq!(0x00000000, super::i(0x00000000, 0x00000000, 0xffffffff));
+        assert_eq!(0x00000000, super::i(0x00000000, 0xffffffff, 0x00000000));
+        assert_eq!(0xffffffff, super::i(0x00000000, 0xffffffff, 0xffffffff));
+        assert_eq!(0xffffffff, super::i(0xffffffff, 0x00000000, 0x00000000));
+        assert_eq!(0xffffffff, super::i(0xffffffff, 0x00000000, 0xffffffff));
+        assert_eq!(0x00000000, super::i(0xffffffff, 0xffffffff, 0x00000000));
+        assert_eq!(0x00000000, super::i(0xffffffff, 0xffffffff, 0xffffffff));
+    }
+}
+
 fn mix_words(_digest: &mut Digest, _x: &[u32]) {
     #[macro_export]
     macro_rules! mix {
@@ -171,86 +175,110 @@ fn mix_words(_digest: &mut Digest, _x: &[u32]) {
                                                           .rotate_left($s));
         };
     }
+    
+    macro_rules! mix_round1 {
+        ($a: ident, $b: ident, $c: ident, $d: ident, $k: literal, $s: literal, $t_i: literal) => {
+           mix!(f, $a, $b, $d, $d, $k, $s, $t_i);
+        };
+    }
 
-    mix!(f, a, b, c, d,  0,  7, 0xd76aa478);
-    mix!(f, d, a, b, c,  1, 12, 0xe8c7b756);
-    mix!(f, c, d, a, b,  2, 17, 0x242070db);
-    mix!(f, b, c, d, a,  3, 22, 0xc1bdceee);
+    mix_round1!(a, b, c, d,  0,  7, 0xd76aa478);
+    mix_round1!(d, a, b, c,  1, 12, 0xe8c7b756);
+    mix_round1!(c, d, a, b,  2, 17, 0x242070db);
+    mix_round1!(b, c, d, a,  3, 22, 0xc1bdceee);
 
-    mix!(f, a, b, c, d,  4,  7, 0xf57c0faf);
-    mix!(f, d, a, b, c,  5, 12, 0x4787c62a);
-    mix!(f, c, d, a, b,  6, 17, 0xa8304613);
-    mix!(f, b, c, d, a,  7, 22, 0xfd469501);
+    mix_round1!(a, b, c, d,  4,  7, 0xf57c0faf);
+    mix_round1!(d, a, b, c,  5, 12, 0x4787c62a);
+    mix_round1!(c, d, a, b,  6, 17, 0xa8304613);
+    mix_round1!(b, c, d, a,  7, 22, 0xfd469501);
 
-    mix!(f, a, b, c, d,  8,  7, 0x698098d8);
-    mix!(f, d, a, b, c,  9, 12, 0x8b44f7af);
-    mix!(f, c, d, a, b, 10, 17, 0xffff5bb1);
-    mix!(f, b, c, d, a, 11, 22, 0x895cd7be);
+    mix_round1!(a, b, c, d,  8,  7, 0x698098d8);
+    mix_round1!(d, a, b, c,  9, 12, 0x8b44f7af);
+    mix_round1!(c, d, a, b, 10, 17, 0xffff5bb1);
+    mix_round1!(b, c, d, a, 11, 22, 0x895cd7be);
 
-    mix!(f, a, b, c, d, 12,  7, 0x6b901122);
-    mix!(f, d, a, b, c, 13, 12, 0xfd987193);
-    mix!(f, c, d, a, b, 14, 17, 0xa679438e);
-    mix!(f, b, c, d, a, 15, 22, 0x49b40821);
+    mix_round1!(a, b, c, d, 12,  7, 0x6b901122);
+    mix_round1!(d, a, b, c, 13, 12, 0xfd987193);
+    mix_round1!(c, d, a, b, 14, 17, 0xa679438e);
+    mix_round1!(b, c, d, a, 15, 22, 0x49b40821);
 
-    mix!(g, a, b, c, d,  1,  5, 0xf61e2562);
-    mix!(g, d, a, b, c,  6,  9, 0xc040b340);
-    mix!(g, c, d, a, b, 11, 14, 0x265e5a51);
-    mix!(g, b, c, d, a,  0, 20, 0xe9b6c7aa);
+    macro_rules! mix_round2 {
+        ($a: ident, $b: ident, $c: ident, $d: ident, $k: literal, $s: literal, $t_i: literal) => {
+           mix!(g, $a, $b, $d, $d, $k, $s, $t_i);
+        };
+    }
 
-    mix!(g, a, b, c, d,  5,  5, 0xd62f105d);
-    mix!(g, d, a, b, c, 10,  9, 0x02441453);
-    mix!(g, c, d, a, b, 15, 14, 0xd8a1e681);
-    mix!(g, b, c, d, a,  4, 20, 0xe7d3fbc8);
+    mix_round2!(a, b, c, d,  1,  5, 0xf61e2562);
+    mix_round2!(d, a, b, c,  6,  9, 0xc040b340);
+    mix_round2!(c, d, a, b, 11, 14, 0x265e5a51);
+    mix_round2!(b, c, d, a,  0, 20, 0xe9b6c7aa);
 
-    mix!(g, a, b, c, d,  9,  5, 0x21e1cde6);
-    mix!(g, d, a, b, c, 14,  9, 0xc33707d6);
-    mix!(g, c, d, a, b,  3, 14, 0xf4d50d87);
-    mix!(g, b, c, d, a,  8, 20, 0x455a14ed);
+    mix_round2!(a, b, c, d,  5,  5, 0xd62f105d);
+    mix_round2!(d, a, b, c, 10,  9, 0x02441453);
+    mix_round2!(c, d, a, b, 15, 14, 0xd8a1e681);
+    mix_round2!(b, c, d, a,  4, 20, 0xe7d3fbc8);
 
-    mix!(g, a, b, c, d, 13,  5, 0xa9e3e905);
-    mix!(g, d, a, b, c,  2,  9, 0xfcefa3f8);
-    mix!(g, c, d, a, b,  7, 14, 0x676f02d9);
-    mix!(g, b, c, d, a, 12, 20, 0x8d2a4c8a);
+    mix_round2!(a, b, c, d,  9,  5, 0x21e1cde6);
+    mix_round2!(d, a, b, c, 14,  9, 0xc33707d6);
+    mix_round2!(c, d, a, b,  3, 14, 0xf4d50d87);
+    mix_round2!(b, c, d, a,  8, 20, 0x455a14ed);
 
-    mix!(h, a, b, c, d,  5,  4, 0xfffa3942);
-    mix!(h, d, a, b, c,  8, 11, 0x8771f681);
-    mix!(h, c, d, a, b, 11, 16, 0x6d9d6122);
-    mix!(h, b, c, d, a, 14, 23, 0xfde5380c);
+    mix_round2!(a, b, c, d, 13,  5, 0xa9e3e905);
+    mix_round2!(d, a, b, c,  2,  9, 0xfcefa3f8);
+    mix_round2!(c, d, a, b,  7, 14, 0x676f02d9);
+    mix_round2!(b, c, d, a, 12, 20, 0x8d2a4c8a);
 
-    mix!(h, a, b, c, d,  1,  4, 0xa4beea44);
-    mix!(h, d, a, b, c,  4, 11, 0x4bdecfa9);
-    mix!(h, c, d, a, b,  7, 16, 0xf6bb4b60);
-    mix!(h, b, c, d, a, 10, 23, 0xbebfbc70);
+    macro_rules! mix_round3 {
+        ($a: ident, $b: ident, $c: ident, $d: ident, $k: literal, $s: literal, $t_i: literal) => {
+           mix!(h, $a, $b, $d, $d, $k, $s, $t_i);
+        };
+    }
 
-    mix!(h, a, b, c, d, 13,  4, 0x289b7ec6);
-    mix!(h, d, a, b, c,  0, 11, 0xeaa127fa);
-    mix!(h, c, d, a, b,  3, 16, 0xd4ef3085);
-    mix!(h, b, c, d, a,  6, 23, 0x04881d05);
+    mix_round3!(a, b, c, d,  5,  4, 0xfffa3942);
+    mix_round3!(d, a, b, c,  8, 11, 0x8771f681);
+    mix_round3!(c, d, a, b, 11, 16, 0x6d9d6122);
+    mix_round3!(b, c, d, a, 14, 23, 0xfde5380c);
 
-    mix!(h, a, b, c, d,  9,  4, 0xd9d4d039);
-    mix!(h, d, a, b, c, 12, 11, 0xe6db99e5);
-    mix!(h, c, d, a, b, 15, 16, 0x1fa27cf8);
-    mix!(h, b, c, d, a,  2, 23, 0xc4ac5665);
+    mix_round3!(a, b, c, d,  1,  4, 0xa4beea44);
+    mix_round3!(d, a, b, c,  4, 11, 0x4bdecfa9);
+    mix_round3!(c, d, a, b,  7, 16, 0xf6bb4b60);
+    mix_round3!(b, c, d, a, 10, 23, 0xbebfbc70);
 
-    mix!(i, a, b, c, d,  0,  6, 0xf4292244);
-    mix!(i, d, a, b, c,  7, 10, 0x432aff97);
-    mix!(i, c, d, a, b, 14, 15, 0xab9423a7);
-    mix!(i, b, c, d, a,  5, 21, 0xfc93a039);
+    mix_round3!(a, b, c, d, 13,  4, 0x289b7ec6);
+    mix_round3!(d, a, b, c,  0, 11, 0xeaa127fa);
+    mix_round3!(c, d, a, b,  3, 16, 0xd4ef3085);
+    mix_round3!(b, c, d, a,  6, 23, 0x04881d05);
 
-    mix!(i, a, b, c, d, 12,  6, 0x655b59c3);
-    mix!(i, d, a, b, c,  3, 10, 0x8f0ccc92);
-    mix!(i, c, d, a, b, 10, 15, 0xffeff47d);
-    mix!(i, b, c, d, a,  1, 21, 0x85845dd1);
+    mix_round3!(a, b, c, d,  9,  4, 0xd9d4d039);
+    mix_round3!(d, a, b, c, 12, 11, 0xe6db99e5);
+    mix_round3!(c, d, a, b, 15, 16, 0x1fa27cf8);
+    mix_round3!(b, c, d, a,  2, 23, 0xc4ac5665);
 
-    mix!(i, a, b, c, d,  8,  6, 0x6fa87e4f);
-    mix!(i, d, a, b, c, 15, 10, 0xfe2ce6e0);
-    mix!(i, c, d, a, b,  6, 15, 0xa3014314);
-    mix!(i, b, c, d, a, 13, 21, 0x4e0811a1);
+    macro_rules! mix_round4 {
+        ($a: ident, $b: ident, $c: ident, $d: ident, $k: literal, $s: literal, $t_i: literal) => {
+           mix!(i, $a, $b, $d, $d, $k, $s, $t_i);
+        };
+    }
 
-    mix!(i, a, b, c, d,  4,  6, 0xf7537e82);
-    mix!(i, d, a, b, c, 11, 10, 0xbd3af235);
-    mix!(i, c, d, a, b,  2, 15, 0x2ad7d2bb);
-    mix!(i, b, c, d, a,  9, 21, 0xeb86d391);
+    mix_round4!(a, b, c, d,  0,  6, 0xf4292244);
+    mix_round4!(d, a, b, c,  7, 10, 0x432aff97);
+    mix_round4!(c, d, a, b, 14, 15, 0xab9423a7);
+    mix_round4!(b, c, d, a,  5, 21, 0xfc93a039);
+
+    mix_round4!(a, b, c, d, 12,  6, 0x655b59c3);
+    mix_round4!(d, a, b, c,  3, 10, 0x8f0ccc92);
+    mix_round4!(c, d, a, b, 10, 15, 0xffeff47d);
+    mix_round4!(b, c, d, a,  1, 21, 0x85845dd1);
+
+    mix_round4!(a, b, c, d,  8,  6, 0x6fa87e4f);
+    mix_round4!(d, a, b, c, 15, 10, 0xfe2ce6e0);
+    mix_round4!(c, d, a, b,  6, 15, 0xa3014314);
+    mix_round4!(b, c, d, a, 13, 21, 0x4e0811a1);
+
+    mix_round4!(a, b, c, d,  4,  6, 0xf7537e82);
+    mix_round4!(d, a, b, c, 11, 10, 0xbd3af235);
+    mix_round4!(c, d, a, b,  2, 15, 0x2ad7d2bb);
+    mix_round4!(b, c, d, a,  9, 21, 0xeb86d391);
 }
 
 fn as_u32(byte1: u8, byte2: u8, byte3: u8, byte4: u8) -> u32 {
